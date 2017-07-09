097f1a2f911cfd69558232431e7c4a1dd86579a9
dragos p <dragospaul@icloud.com>
2017-07-09 13:06:49 +0300

    add file

---
+++ public/js/ng-controllers/userProfileController.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('userProfileController', userProfileController);
+
+    userProfileController.$inject = ['$rootScope', '$scope', '$state', '$http', '$stateParams'];
+    function userProfileController($rootScope, $scope, $state, $http, $stateParams) {
+
+        (function initController() {
+            $http({
+                method: 'GET',
+                url: "/user/" + $rootScope.auth.userId
+            }).then(function successCallback(response) {
+                $scope.user = response.data[0];
+            }, function errorCallback(response) {
+            });
+        })();
+
+        $scope.editProfile = function (user) {
+            user.user_id = $rootScope.auth.userId;
+            $http({
+                method: 'POST',
+                url: "/user/" + $rootScope.auth.userId,
+                data: user
+            }).then(function successCallback(response) {
+                alert("ok");
+            }, function errorCallback(response) {
+                alert("err");
+            });
+        }
+    }
+
+
+})();
